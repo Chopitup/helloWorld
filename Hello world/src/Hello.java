8d61dd50e6f3a3b39b02829c259fbdee34a76933
pabloalvarez <pabloalvarez@pablos-mac-mini.attlocal.net>
2022-07-09 14:18:27 -0500

    First commit

---
+++ Hello world/src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		String var = "Hello worldss";
+		int num = 10;
+		System.out.println(num);
+	}
+
+}
+
